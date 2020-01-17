31214da67b9264ac5924db49bc5f5c6257922575
Romel Fernando <romelfernando09@gmail.com>
2020-01-17 23:04:26 +0800

    fix heroku setup

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from main import app, db
 import config
 
 # load the configurations
-app.config.from_object('config.HerokuProdConfig')
+app.config.from_object('config.DevConfig')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
